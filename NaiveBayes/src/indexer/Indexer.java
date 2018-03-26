89d965b3f3eae38f365f645151aebc1b012c508c
chuzhumin98 <chuzhumin98@gmail.com>
2018-03-26 12:00:57 +0800

    calculate information gain

--- NaiveBayes/src/indexer/Indexer.java
+++ NaiveBayes/src/indexer/Indexer.java
@@ -168,6 +168,37 @@ public class Indexer {
 			this.gain = 0.0;
 		}
 		
+		/**
+		 * 计算信息增益
+		 * 
+		 * @param spamTotal
+		 * @param hamTotal
+		 */
+		void calculateInfoGain(int spamTotal, int hamTotal) {
+			double entropy = this.calcalateEntropy(spamTotal, spamTotal+hamTotal);
+			double newEntropy = spamTotal * this.calcalateEntropy(this.spamNum, spamTotal) / (spamTotal+hamTotal)
+					+ hamTotal * this.calcalateEntropy(this.hamNum, hamTotal) / (spamTotal+hamTotal);
+			this.gain = entropy - newEntropy; //信息增益为两者之差
+		}
 		
+		/**
+		 * 计算信息熵
+		 * 
+		 * @param n1
+		 * @param N
+		 * @return
+		 */
+		double calcalateEntropy(int n1, int N) {
+			double p = 1.0*n1/N;
+			double q = 1 - p;
+			double entropy = 0.0;
+			if (p > 1e-5) { //防止碰到0/0的情况
+				entropy -= p * Math.log(p) / Math.log(2);
+			}
+			if (q > 1e-5) {
+				entropy -= q * Math.log(q) / Math.log(2);
+			}
+			return entropy;
+		}
 	}
 }
