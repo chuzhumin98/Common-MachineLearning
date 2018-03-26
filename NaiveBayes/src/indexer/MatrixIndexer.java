5f0397800bf25a31da545e9ad1eaf0d8c10c9045
chuzhumin98 <chuzhumin98@gmail.com>
2018-03-26 20:37:54 +0800

    end for matrix building

--- NaiveBayes/src/indexer/MatrixIndexer.java
+++ NaiveBayes/src/indexer/MatrixIndexer.java
@@ -2,6 +2,7 @@ package indexer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -32,11 +33,58 @@ public class MatrixIndexer {
 			/**
 			 * 开始文档的读取和特征的写入
 			 */
-			
+			for (int i = 0; i < this.wordIndex.indexPathes.size(); i++) {
+				//int i = 0;
+				String path = this.wordIndex.indexPathes.get(i);
+				//System.out.println("current path: "+path);
+				Scanner input1 = new Scanner(new File(path));
+				//进入正文之前
+				while (input1.hasNextLine()) {
+					String line = input1.nextLine();
+					if (line.length() == 0) {
+						break;
+					}			
+				}
+				//进入正文之后
+				while (input1.hasNextLine()) {
+					String line = input1.nextLine();
+					String[] splits = line.split(" ");
+					for (int j = 0; j < splits.length; j++) {
+						if (topWordCount.containsKey(splits[j])) {
+							int count = topWordCount.get(splits[j])+1;
+							topWordCount.put(splits[j], count);
+						}
+					}
+					//System.out.println(line);
+				}
+				input1.close();
+				//将这个文档相关数据写入文件中，并将计数置为0
+				String thisData = ""; //记录这个文档的相关特征数据
+				for (Map.Entry<String, Integer> item: topWordCount.entrySet()) {
+					thisData += item.getValue()+" ";
+				}
+				if (this.wordIndex.isSpam.get(i)) {
+					thisData += "1";
+				} else {
+					thisData += "0";
+				}
+				output.println(thisData);
+				for (Map.Entry<String, Integer> item: topWordCount.entrySet()) {
+					topWordCount.put(item.getKey(), 0); //初始化
+				}
+				if ((i+1) % 1000 == 0) {
+					System.out.println("has index "+(i+1)+" docs.");
+				}
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String[] args) {
+		MatrixIndexer matrix1 = new MatrixIndexer();
+		matrix1.setFeatureMatrix();
 	}
 	
 }
