551fdf764ee933e8462d7c08b6c1870877326331
chuzhumin98 <chuzhumin98@gmail.com>
2018-03-27 08:27:59 +0800

    add for more test model

--- NaiveBayes/src/bayes/NaiveBayes.java
+++ NaiveBayes/src/bayes/NaiveBayes.java
@@ -13,6 +13,7 @@ public class NaiveBayes {
 	public int docSize = 64620; //总的文档个数
 	public static final int trainSize = 45000; //训练集数据的大小
 	public static final int testStart = 45001; //测试集数据开始位置
+	public double alpha = 1.0 / Math.sqrt(trainSize); //设置平滑系数
 	public ArrayList<Entity> entities = new ArrayList<Entity>(); //存储实体集
 	public int featureNum; //特征的维度
 	public ArrayList<Integer> sampleIndex; //样本的索引，前trainsize个为训练集，testsize之后为测试集
@@ -20,7 +21,7 @@ public class NaiveBayes {
 	public int[] classCount; //记录各类训练集总个数，其中0为ham,1为spam
 	
 	public NaiveBayes() {
-		//this.LoadMatrix();
+		this.LoadMatrix();
 		this.shuffleSample();
 	}
 	
@@ -113,9 +114,135 @@ public class NaiveBayes {
 		System.out.println();
 	}
 	
+	/**
+	 * index在[low,high)之间的样本
+	 * 不带加权信息的，普通Naive Bayes
+	 * 返回正确分类的个数
+	 * 
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	public int testModel1(int low, int high) {
+		int countRight = 0;
+		for (int i = low; i < high; i++) {
+			int index = this.sampleIndex.get(i); //随机化的第i个样本点
+			Entity tempEntity = this.entities.get(index);
+			double[] postProb = new double [2]; //对数化的后验概率
+			for (int j = 0; j < 2; j++) {
+				postProb[j] = Math.log(this.classCount[j]*1.0/this.trainSize); //先验概率
+			}
+			for (int j = 0; j < this.featureNum; j++) {
+				if (tempEntity.feature[j] > 0) {
+					for (int k = 0; k < 2; k++) {
+						double prob = 1.0*(this.featureCount[k][j]+this.alpha)
+								/(this.classCount[k]+NaiveBayes.trainSize*this.alpha); //平滑后的概率
+						postProb[k] += Math.log(prob); //计算对数化后的概率
+					}
+				}
+			}
+			int myLabel = 0;
+			if (postProb[1] > postProb[0]) {
+				myLabel = 1;
+			}
+			if (tempEntity.label == myLabel) {
+				countRight++;
+			}
+		}
+		return countRight;
+	}
+	
+	/**
+	 * index在[low,high)之间的样本
+	 * 加权信息的，普通Naive Bayes
+	 * 返回正确分类的个数
+	 * 
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	public int testModel2(int low, int high) {
+		int countRight = 0;
+		for (int i = low; i < high; i++) {
+			int index = this.sampleIndex.get(i); //随机化的第i个样本点
+			Entity tempEntity = this.entities.get(index);
+			double[] postProb = new double [2]; //对数化的后验概率
+			for (int j = 0; j < 2; j++) {
+				postProb[j] = Math.log(this.classCount[j]*1.0/this.trainSize); //先验概率
+			}
+			for (int j = 0; j < this.featureNum; j++) {
+				if (tempEntity.feature[j] > 0) {
+					for (int k = 0; k < 2; k++) {
+						double prob = 1.0*(this.featureCount[k][j]+this.alpha)
+								/(this.classCount[k]+NaiveBayes.trainSize*this.alpha); //平滑后的概率
+						postProb[k] += tempEntity.feature[j] * Math.log(prob); //计算对数化后的概率
+					}
+				}
+			}
+			int myLabel = 0;
+			if (postProb[1] > postProb[0]) {
+				myLabel = 1;
+			}
+			if (tempEntity.label == myLabel) {
+				countRight++;
+			}
+		}
+		return countRight;
+	}
+	
+	/**
+	 * index在[low,high)之间的样本
+	 * 不带加权信息的，但带负面信息的Naive Bayes
+	 * 返回正确分类的个数
+	 * 
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	public int testModel3(int low, int high) {
+		int countRight = 0;
+		for (int i = low; i < high; i++) {
+			int index = this.sampleIndex.get(i); //随机化的第i个样本点
+			Entity tempEntity = this.entities.get(index);
+			double[] postProb = new double [2]; //对数化的后验概率
+			for (int j = 0; j < 2; j++) {
+				postProb[j] = Math.log(this.classCount[j]*1.0/this.trainSize); //先验概率
+			}
+			for (int j = 0; j < this.featureNum; j++) {
+				if (tempEntity.feature[j] > 0) {
+					for (int k = 0; k < 2; k++) {
+						double prob = 1.0*(this.featureCount[k][j]+this.alpha)
+								/(this.classCount[k]+NaiveBayes.trainSize*this.alpha); //平滑后的概率
+						postProb[k] += Math.log(prob); //计算对数化后的概率
+					}
+				} else { //没有出现该词的时候利用反面信息
+					for (int k = 0; k < 2; k++) {
+						double prob = 1.0*(this.classCount[k]-this.featureCount[k][j]+this.alpha)
+								/(this.classCount[k]+NaiveBayes.trainSize*this.alpha); //平滑后的概率
+						postProb[k] += Math.log(prob);
+					}
+				}
+			}
+			int myLabel = 0;
+			if (postProb[1] > postProb[0]) {
+				myLabel = 1;
+			}
+			if (tempEntity.label == myLabel) {
+				countRight++;
+			}
+		}
+		return countRight;
+	}
+	
 	public static void main(String[] args) {
 		NaiveBayes bayes = new NaiveBayes();
-		
+		bayes.trainModel();
+		//int low = 0;
+		//int high = bayes.trainSize;
+		int low = bayes.testStart;
+		int high = bayes.docSize;
+		int right = bayes.testModel3(low, high);
+		System.out.println("correct "+right+" of "+(high-low)+", eta = "+(right*1.0/(high-low)));
 	}
 	
 	public class Entity {
