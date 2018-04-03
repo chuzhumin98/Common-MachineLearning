23a1dae927373608378bf9426467e6dd6c29c3d3
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-03 18:06:52 +0800

    compare models

--- NaiveBayes/result/train size/compare.m
+++ NaiveBayes/result/train size/compare.m
@@ -1,9 +1,10 @@
 clear all
+%% 计算模型的置信区间
 [error] = textread('compare.txt');
 errorMean(1) = mean(error(:,1)); %计算误差的平均值
 errorMean(2) = mean(error(:,2));
 size = length(error);
-alpha = 0.05;
+alpha = 0.01;
 z = norminv(1-alpha/2,0,1);
 CI = zeros(2,2);
 for i = 1:2
@@ -11,3 +12,7 @@ for i = 1:2
     CI(i,1) = errorMean(i)-delta;
     CI(i,2) = errorMean(i)+delta;
 end
+%% 进行模型之间的比较
+sigma = sqrt(errorMean(1)*(1-errorMean(1))/size+errorMean(2)*(1-errorMean(2))/size);
+confidence1to2 = normcdf((errorMean(2)-errorMean(1))/sigma, 0, 1);
+confidence2to1 = normcdf((errorMean(1)-errorMean(2))/sigma, 0, 1);
