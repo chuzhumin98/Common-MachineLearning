f3f455c93d676b941399d86fa7f8ab37b980328f
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-04 11:38:55 +0800

    fix the error part bug

--- NaiveBayes/result/train size/compare.m
+++ NaiveBayes/result/train size/compare.m
@@ -1,18 +1,17 @@
 clear all
 %% 计算模型的置信区间
 [error] = textread('compare.txt');
-errorMean(1) = mean(error(:,1)); %计算误差的平均值
-errorMean(2) = mean(error(:,2));
-size = length(error);
-alpha = 0.01;
+size(1) = 64620*19/20; %0.05的训练集
+size(2) = 64620/2; %0.50的训练集
+alpha = 0.1;
 z = norminv(1-alpha/2,0,1);
 CI = zeros(2,2);
 for i = 1:2
-    delta = z*sqrt(errorMean(i)*(1-errorMean(i))/size);
-    CI(i,1) = errorMean(i)-delta;
-    CI(i,2) = errorMean(i)+delta;
+    delta = z*sqrt(error(i)*(1-error(i))/size(i));
+    CI(i,1) = error(i)-delta;
+    CI(i,2) = error(i)+delta;
 end
 %% 进行模型之间的比较
-sigma = sqrt(errorMean(1)*(1-errorMean(1))/size+errorMean(2)*(1-errorMean(2))/size);
-confidence1to2 = normcdf((errorMean(2)-errorMean(1))/sigma, 0, 1);
-confidence2to1 = normcdf((errorMean(1)-errorMean(2))/sigma, 0, 1);
+sigma = sqrt(error(1)*(1-error(1))/size(1)+error(2)*(1-error(2))/size(2));
+confidence1to2 = normcdf((error(2)-error(1))/sigma, 0, 1);
+confidence2to1 = normcdf((error(1)-error(2))/sigma, 0, 1);
