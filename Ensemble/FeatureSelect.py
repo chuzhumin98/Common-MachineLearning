eb88cadec33181aa2966c6a26550255b5592229f
chuzhumin98 <chuzhumin98@gmail.com>
2018-05-09 11:20:20 +0800

    output for top-k useful words

--- Ensemble/FeatureSelect.py
+++ Ensemble/FeatureSelect.py
@@ -29,18 +29,43 @@ def selectTopWords(dataframe, k):
     labelCounts[0] = len(dataframe.loc[(dataframe['label'] == 1)])
     labelCounts[1] = len(dataframe.loc[(dataframe['label'] == 0)])
     labelCounts[2] = len(dataframe.loc[(dataframe['label'] == -1)])
+    totalAverage = (labelCounts[0] - labelCounts[2]) / len(totalWords)
     print(labelCounts)
+    print(totalAverage)
+    infoGains = [] #计算各词汇的信息量(采用标签delta法计算信息量）
     for i in range(len(totalWords)):
-        dfTemp = dataframe.loc[(dataframe['review'].str.contains(totalWords[i]))]
+        #print(totalWords[i])
         countIn = np.zeros([3])
-        print(len(dfTemp.loc[(dataframe['label'] == 1)]))
-        print(len(dfTemp.loc[(dataframe['label'] == 0)]))
-        print(len(dfTemp.loc[(dataframe['label'] == -1)]))
-    print(totalWords)
-    print(len(totalWords))
+        try:
+            dfTemp = dataframe.loc[(dataframe['review'].str.contains(totalWords[i]))]
+            countIn[0] = len(dfTemp.loc[(dataframe['label'] == 1)])
+            countIn[1] = len(dfTemp.loc[(dataframe['label'] == 0)])
+            countIn[2] = len(dfTemp.loc[(dataframe['label'] == -1)])
+        except:
+            countIn = np.zeros([3])
+            print('error in word ',totalWords[i])
+        totalNum = sum(countIn)
+        if (totalNum == 0):
+            infoGains.append(0)
+        else:
+            averageIn = (countIn[0] - countIn[2]) / totalNum  # 含有这一类的平均得分
+            deltaScore = abs(averageIn - totalAverage)
+            infoGains.append(deltaScore * totalNum)
+        if ((i+1) % 100 == 0):
+            print(infoGains[-100:])
+            print('complete ',(i+1), ' of ',len(totalWords))
+    infoGains = np.array(infoGains)
+    topkIndex = np.argpartition(-infoGains, k-1)[0:k] #找到topk信息量的词汇
+    usefulWords = totalWords[topkIndex]
+    # 将数据导出到文件中
+    f = open("usefulWords1000.npy", "wb")
+    np.save(f, usefulWords)
+    f.close()
+    return usefulWords
 
 if __name__ == '__main__':
     trainFilePath = 'exp2.train.csv'
     df1 = pd.read_csv(trainFilePath, encoding='utf-8')
     #recordTotalWords(df1)
-    selectTopWords(df1, 100)
+    usefulWords = selectTopWords(df1, 1000)
+    print(usefulWords)
