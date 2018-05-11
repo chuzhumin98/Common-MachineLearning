917d64892144802b1a72aa63bb0cc4a24f137c1e
chuzhumin98 <chuzhumin98@gmail.com>
2018-05-11 11:24:41 +0800

    split for train and validate set

--- Ensemble/Bagging.py
+++ Ensemble/Bagging.py
@@ -20,24 +20,43 @@ def evaluateResult(label, predictLabel):
         print('error for label and predictLabel is not equavalant length')
         return 2
     else:
-        RMSE = math.sqrt(np.sum(np.square(np.subtract(label-predictResult))/len(label)))
+        RMSE = math.sqrt(np.mean(np.square(np.subtract(label,predictResult))))
         return RMSE
+
+#随机划分训练集和验证集
+#    参数samples：所有样本数据
+#    参数samplesLabels：样本所对应的标签
+#    return：[训练集数据, 训练集标签, 验证集数据, 验证集标签]
+def splitDatas(samples, samplesLabels):
+    size = len(samplesLabels) #总的样本点个数
+    indexArray = np.array(range(size), dtype=int) #下标数组
+    np.random.shuffle(indexArray)
+    validateStart = size * 4 // 5 #按照4:1的比例划分划分训练集和验证集
+    trainData = samples[indexArray[0:validateStart]]
+    trainLabel = samplesLabels[indexArray[0:validateStart]]
+    validateData = samples[indexArray[validateStart:size]]
+    validateLabel = samplesLabels[indexArray[validateStart:size]]
+    return [trainData, trainLabel, validateData, validateLabel]
 
 
 if __name__ == '__main__':
     # 导入数据部分
     trainFilePath = 'exp2.train.csv'
     df1 = pd.read_csv(trainFilePath, encoding='utf-8')
-    trainLabels = df1['label']
-    trainAppearMatrix = loadData('matrix200.npy')
-    testAppearMatrix = loadData('testMatrix200.npy')
+    trainLabels = np.array(df1['label'])
+    trainAppearMatrix = loadData('matrix1000.npy')
+    trainAppearMatrix, trainLabels, validateAppearMatrix, validateLabels = splitDatas(trainAppearMatrix, trainLabels)
+    testAppearMatrix = loadData('testMatrix1000.npy')
+    print('succeed load data')
     # 进行模型训练训练和预测部分
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(trainAppearMatrix, trainLabels)
-    predictResult = clf.predict(testAppearMatrix)
+    predictResult = clf.predict(validateAppearMatrix)
     print(predictResult)
-    print('error:',np.sum(np.abs(np.subtract(predictResult, df1['label']))))
+    print(validateLabels)
+    print('RMSE in validateSet:', evaluateResult(validateLabels, predictResult))
     # 结果导出部分
-    #exportResult(predictResult, 'result/decision_tree_baseline1000.csv')
+    predictResult = clf.predict(testAppearMatrix)
+    exportResult(predictResult, 'result/decision_tree_baseline1000_0_8.csv')
 
 
