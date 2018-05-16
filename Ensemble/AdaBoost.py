e07337ad6250e02768fe947335350fca5452eedf
chuzhumin98 <chuzhumin98@gmail.com>
2018-05-16 19:01:40 +0800

    final sumbit version

--- Ensemble/AdaBoost.py
+++ Ensemble/AdaBoost.py
@@ -89,11 +89,11 @@ if __name__ == '__main__':
     print('succeed load data')
     # 进行模型训练训练和预测部分
     trainAppearMatrix, trainLabels, validateAppearMatrix, validateLabels = splitDatas(trainAppearMatrix0, trainLabels0)
-    #predictResult = AdaBoost(trainAppearMatrix, trainLabels, validateAppearMatrix, 20, 0, maxDepth=None) #使用AdaBoost进行分类
-    #print('RMSE in validateSet:', evaluateResult(validateLabels, predictResult))
+    predictResult = AdaBoost(trainAppearMatrix, trainLabels, validateAppearMatrix, 20, 0, maxDepth=None) #使用AdaBoost进行分类
+    print('RMSE in validateSet:', evaluateResult(validateLabels, predictResult))
     # 将预测结果输出
-    #predictResult = AdaBoost(trainAppearMatrix0, trainLabels0, testAppearMatrix, 20, 0, maxDepth=None)  # 使用AdaBoost进行分类
-    #exportResult(predictResult, 'result/decision_tree_20AdaBoost1000_v2.csv')
+    predictResult = AdaBoost(trainAppearMatrix0, trainLabels0, testAppearMatrix, 20, 0, maxDepth=None)  # 使用AdaBoost进行分类
+    exportResult(predictResult, 'result/decision_tree_20AdaBoost1000_v2.csv')
     methodLists = [1,0,3,2,5,6,4,7]
     predictResult = commonAdaBoost(trainAppearMatrix0, trainLabels0, testAppearMatrix, methodLists)
     exportResult(predictResult, 'result/AdaBoost_[1,0,3,2,5,6,4,7]_v2.csv')
